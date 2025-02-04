dc9d42b73f70efd5b9b5f8270ae7d5459ded1c7d
JK-But-Cool <jkclumagui@gmail.com>
2025-02-04 07:50:33 -0800

    Create count using while loop even though a for loop would be better for this specific scenario.py

---
+++ count using while loop even though a for loop would be better for this specific scenario.py
@@ -0,0 +1,9 @@
+def main():
+    print("Starting While Loop - Print Count Variable")
+    nerdemoji = 0
+    # Enter Your Code Here:
+    while nerdemoji <= 9:
+      print(nerdemoji)
+      nerdemoji += 1
+    print("Ending While Loop")
+main()
