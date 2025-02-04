34cc4be226f72e1b9aa5640fce29ac8332f83da3
JK-But-Cool <jkclumagui@gmail.com>
2025-02-04 07:56:16 -0800

    Update count using while loop even though a for loop would be better for this specific scenario.py

--- count using while loop even though a for loop would be better for this specific scenario.py
+++ count using while loop even though a for loop would be better for this specific scenario.py
@@ -1,9 +1,21 @@
 def main():
-    print("Starting While Loop - Print Count Variable")
-    nerdemoji = 0
+
+  print("Starting While Loop - Print Count Variable")
+  flag = False
+  while flag == False:
+    try:
+      nedemoji = int(input("number please: "))
+    except:
+      print ("TYPE A NUMBER!!!!!!!!!")
+    else:
+      flag = True
+  nerdemoji = 0
     # Enter Your Code Here:
-    while nerdemoji <= 9:
-      print(nerdemoji)
-      nerdemoji += 1
-    print("Ending While Loop")
+  while nerdemoji <= 9:
+    print(nerdemoji)
+    if(nerdemoji == nedemoji):
+      print("hey wait a minute,",nedemoji,"is also your number!")
+    nerdemoji += 1
+  print("Ending While Loop")
+
 main()
