f756eabf1ea62351556ce51af47504162ae4bd84
JK-But-Cool <jkclumagui@gmail.com>
2025-02-05 09:22:25 -0800

    Create stringbean.py

---
+++ stringbean.py
@@ -0,0 +1,7 @@
+my_string = input('gimme a string: ')
+print('The variable "my_string" = ',my_string)
+biggest = max(my_string)
+print('The biggest character in the \nstring "STEAM Is Cool" is','"'+biggest+'".')
+smallest = min(my_string)
+print('The smallest character in the \nstring "STEAM Is Cool" is','"'+smallest+'".')
+# print('Done with Challenge 1')
