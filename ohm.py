1e6604b03323b11eb1d37a18112cc7dfb960dc05
JK-But-Cool <jkclumagui@gmail.com>
2025-02-05 10:23:14 -0800

    Create ohm.py

---
+++ ohm.py
@@ -0,0 +1,15 @@
+print(ord('Ω'))
+def diy():
+   print("i'm feelin lazy... you make the list for me.")
+   flag = False
+   while flag == False:
+    try:
+        v = float(input("how much voltage do you have? "))
+        i = float(input("how many amps do you want? "))
+    except:
+        print ("please only input numbers")
+    else:
+        flag = True
+        o = v/i
+        print("you will need a",str(o)+chr(937),"resistor")
+diy()
